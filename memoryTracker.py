9be7485036bb790cec0f47a8740762495192fb5c
Billy Liggins <billy.liggins@qmul.ac.uk>
2015-11-17 12:50:47 +0000

    You have sorted out the PID finder

--- memoryTracker.py
+++ memoryTracker.py
@@ -10,23 +10,31 @@ def getPID():
     for line in qstatString:
         lineSplit=line.split()
         PID.append(lineSplit[0])
-    print PID
-    print os.system("qstat | awk ' {print $1}' ")
+    print PID[2:]
+    #print os.system("qstat | awk ' {print $1}' ")
 
-    for i in len(PID):
-        print float(PID[i])
+#    for i in PID:
+#        #print float(PID)
+#        print "the is the elememnt " ,i
+    
+    return PID[2:]
+
 
 def trackMemory(pid):
     for id in pid:
-        os.system("qstat -j "+str(id[0])+" >> temp.info")
+        print id
+        os.system("qstat -j "+str(id)+" > temp.info")
         memInfo=open("temp.info", "r").read()
+        print memInfo
         ''' well you need to now go through the memInfo string
                 find the memory info and then write this to a file... then wait for 5 mins'''
-
+        print "You have finished the loop"
 
 if __name__=="__main__":
     
-    getPID()
+    pid=getPID()
+    print "len of PID ", len(pid)
+    trackMemory(pid)
 
 
 
