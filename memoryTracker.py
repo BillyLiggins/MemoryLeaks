72e4356e5971b2cc75a394639f9ee6e19cf858be
Billy Liggins <billy.liggins@qmul.ac.uk>
2015-11-17 12:25:43 +0000

    You have started making the memory checker method

--- memoryTracker.py
+++ memoryTracker.py
@@ -8,16 +8,28 @@ def getPID():
     print qstatString[0]
     PID=[] 
     for line in qstatString:
-     lineSplit=line.split()
-     PID.append(lineSplit[0])
+        lineSplit=line.split()
+        PID.append(lineSplit[0])
     print PID
     print os.system("qstat | awk ' {print $1}' ")
 
+    for i in len(PID):
+        print float(PID[i])
 
-    print float(PID[3])
-
+def trackMemory(pid):
+    for id in pid:
+        os.system("qstat -j "+str(id[0])+" >> temp.info")
+        memInfo=open("temp.info", "r").read()
+        ''' well you need to now go through the memInfo string
+                find the memory info and then write this to a file... then wait for 5 mins'''
 
 
 if __name__=="__main__":
     
     getPID()
+
+
+
+        
+
+
