81d5be7465beefc56ce4e779086182b0624de8ea
Billy Liggins <billy.liggins@qmul.ac.uk>
2015-11-18 11:05:05 +0000

    the memory tracker method is half way towards being able to read the memory used

--- memoryTracker.py
+++ memoryTracker.py
@@ -10,34 +10,29 @@ def getPID():
     for line in qstatString:
         lineSplit=line.split()
         PID.append(lineSplit[0])
-    print PID[2:]
-    #print os.system("qstat | awk ' {print $1}' ")
-
-#    for i in PID:
-#        #print float(PID)
-#        print "the is the elememnt " ,i
     
+    print PID[2:]
     return PID[2:]
 
 
 def trackMemory(pid):
     for id in pid:
-        print id
+        #print id
         os.system("qstat -j "+str(id)+" > temp.info")
-        memInfo=open("temp.info", "r").read()
-        print memInfo
+        memInfo=open("temp.info", "r")
+        with open('temp.info') as f:
+           for line in f:
+                if "maxvmem" in line:
+                    print line.split()[7][8:-1]
+                                    
+                if 'str' in line:
+                        break
+        #print memInfo
         ''' well you need to now go through the memInfo string
                 find the memory info and then write this to a file... then wait for 5 mins'''
         print "You have finished the loop"
-
 if __name__=="__main__":
     
     pid=getPID()
     print "len of PID ", len(pid)
     trackMemory(pid)
-
-
-
-        
-
-
