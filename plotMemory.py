955241487eb74f3a04d4341f4c8cdba11ac7e0f1
Billy Liggins <billy.liggins@qmul.ac.uk>
2015-11-18 13:21:52 +0000

    primative plotting in matplotlib now added to repo

---
+++ plotMemory.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import pylab
+
+
+data = pylab.loadtxt("memoryData.dat")
+print data
+pylab.plot( data[1:])
+
+pylab.legend()
+pylab.title("Memory over time")
+pylab.xlabel("Time (30 sec steps)")
+pylab.ylabel("Memory Usage (Gb)")
+pylab.show()
